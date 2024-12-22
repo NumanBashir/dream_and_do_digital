489aa2d573d478b6d6491963231259d11fd48864
NumanBashir <sportybashir@gmail.com>
2024-12-22 18:45:50 +0100

    nav bar in testing

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,7 +1,40 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { addDays, format } from "date-fns";
+import { log } from "console";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  const currentDate = new Date();
+  const formatedDate = format(currentDate, "dd MMMM, yyyy");
+
+  return (
+    <nav className="flex-between w-full mb-8 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src="/assets/book-svgrepo-com.svg"
+          alt="Dream & Do Digital Logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
+        <p>Dream & Do Digital</p>
+      </Link>
+      <div className="sm:flex hidden">
+        <div className="flex gap-3 md:gap-5">
+          <Link href="/" className="blue_btn">
+            {formatedDate}
+          </Link>
+          <Link href="/services" className="blue_btn">
+            Current Week
+          </Link>
+          <Link href="/contact" className="blue_btn">
+            Current Month
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
