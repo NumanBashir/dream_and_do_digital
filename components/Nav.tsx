b508c53b0833c8ae9f4a89ac13a6876f4da6821a
NumanBashir <sportybashir@gmail.com>
2024-12-24 14:09:58 +0100

    new design + library

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -9,17 +9,7 @@ const Nav = () => {
   const formatedDate = format(currentDate, "dd MMMM, yyyy");
 
   return (
-    <nav className="flex-between w-full mb-8 pt-3">
-      <Link href="/" className="flex gap-2 flex-center">
-        <Image
-          src="/assets/book-svgrepo-com.svg"
-          alt="Dream & Do Digital Logo"
-          width={30}
-          height={30}
-          className="object-contain"
-        />
-        <p>Dream & Do Digital</p>
-      </Link>
+    <nav className="flex-center w-full mb-8 pt-3">
       <div className="sm:flex hidden">
         <div className="flex gap-3 md:gap-5">
           <Link href="/" className="blue_btn">
