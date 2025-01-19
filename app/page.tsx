81d49f0f52dc4f7154bcd6a0e7d5613aa15e2e2a
NumanBashir <sportybashir@gmail.com>
2025-01-19 17:30:20 +0100

    design of cardview

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,45 @@
-import Image from "next/image";
+import DayView from "@/components/DayView";
 
 export default function Home() {
   return (
-    <section className="w-full flex-center flex-col text-5xl">
-      <h1 className="font-medium">Welcome to Dream & Do Digital</h1>
-      <div className="flex w-full gap-4 mt-8">
-        <div className="basis-1/3 border-2 border-red-600 h-96">
-          <div className="flex flex-col gap-4 px-4">
-            <div>
-              <span className="text-lg">🎯 Ugens målsætninger:</span>
-              <ul className="list-disc list-inside text-sm">
-                <li>🏃 Løb 10 km</li>
-                <li>📚 Læs 100 sider</li>
-                <li>🍎 Spis sundt</li>
-              </ul>
-            </div>
+    <>
+      <section className="w-full flex-center flex-col">
+        <h1 className="font-medium text-5xl">Welcome to Dream & Do Digital</h1>
+        <div className="flex w-full gap-4 mt-8">
+          <div className="basis-1/3 border-2 border-red-600 h-96">
+            <div className="flex flex-col gap-4 px-4">
+              <div>
+                <span className="text-lg">🎯 Ugens målsætninger:</span>
+                <ul className="list-disc list-inside text-sm">
+                  <li>🏃 Løb 10 km</li>
+                  <li>📚 Læs 100 sider</li>
+                  <li>🍎 Spis sundt</li>
+                </ul>
+              </div>
 
-            <div>
-              <span className="text-lg">🏋️ Ugens udfordringer:</span>
-              <ul className="list-disc list-inside text-sm">
-                <li>📱 Ingen sociale medier</li>
-                <li>📺 Ingen TV</li>
-                <li>🍭 Ingen slik</li>
-              </ul>
+              <div>
+                <span className="text-lg">🏋️ Ugens udfordringer:</span>
+                <ul className="list-disc list-inside text-sm">
+                  <li>📱 Ingen sociale medier</li>
+                  <li>📺 Ingen TV</li>
+                  <li>🍭 Ingen slik</li>
+                </ul>
+              </div>
+              <span className="text-base"></span>
             </div>
-            <span className="text-base"></span>
           </div>
+          <div className="basis-2/3 border-2 border-indigo-600 h-96"></div>
         </div>
-        <div className="basis-2/3 border-2 border-indigo-600 h-96"></div>
+      </section>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 justify-items-center">
+        <DayView />
+        <DayView />
+        <DayView />
+        <DayView />
+        <DayView />
+        <DayView />
+        <DayView />
       </div>
-    </section>
+    </>
   );
 }
---
+++ components/DayView.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const DayView = () => {
+  return (
+    <div>
+      <div className="card bg-base-100 w-60 shadow-xl my-4">
+        <div className="bg-blue-500 text-white py-4 px-6 rounded-t-lg">
+          <h2 className="text-lg font-semibold flex-center">Mandag</h2>
+        </div>
+
+        {/* Todos section */}
+        <div className="p-4 space-y-3">
+          <h1 className="text-lg font-bold">Daily Todos</h1>
+          <label className="flex items-center space-x-2">
+            <input type="checkbox" className="checkbox checkbox-primary" />
+            <span>Gør værelse rent</span>
+          </label>
+          <label className="flex items-center space-x-2">
+            <input type="checkbox" className="checkbox checkbox-primary" />
+            <span>Handle ind</span>
+          </label>
+        </div>
+
+        <div className="divider"></div>
+
+        {/* Reflection section */}
+        <div className="p-4 space-y-3">
+          <h1 className="text-lg font-bold">Todays reflection ☀️</h1>
+          <p>
+            Today was very good and I kept my focus high and did not eat any
+            sugar or junk food
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DayView;
