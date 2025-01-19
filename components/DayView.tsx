03c6b0fdd592716c3bd0158e99a913f591732655
NumanBashir <sportybashir@gmail.com>
2025-01-19 19:37:00 +0100

    working on changing reflection field

--- components/DayView.tsx
+++ components/DayView.tsx
@@ -1,9 +1,18 @@
-import React from "react";
-import { addDays, format } from "date-fns";
+"use client";
+
+import { useState } from "react";
+import { format } from "date-fns";
 
 const DayView = () => {
   const currentDate = new Date();
   const formatedDate = format(currentDate, "eeee, dd/MM");
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [reflection, setReflection] = useState("Today was a very good day");
+
+  const handleBlur = () => {
+    setIsEditing(false);
+  };
 
   return (
     <div>
@@ -30,10 +39,23 @@ const DayView = () => {
         {/* Reflection section */}
         <div className="p-4 space-y-3">
           <h1 className="text-lg font-bold">Todays reflection ☀️</h1>
-          <p>
-            Today was very good and I kept my focus high and did not eat any
-            sugar or junk food
-          </p>
+          {isEditing ? (
+            <textarea
+              className="w-full border-2 border-blue-500 rounded h-20 hover:border-solid"
+              value={reflection}
+              onChange={(e) => setReflection(e.target.value)}
+              onBlur={handleBlur}
+              rows={10}
+              autoFocus
+            />
+          ) : (
+            <p
+              className="hover:border-solid border-2 border-transparent cursor-pointer"
+              onClick={() => setIsEditing(true)}
+            >
+              {reflection}
+            </p>
+          )}
         </div>
       </div>
     </div>
